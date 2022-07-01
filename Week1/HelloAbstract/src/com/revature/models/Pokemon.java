5a789ce0fc44ccef4e6a2c33fa66adb09e0ec126
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-01 10:43:19 -0400

    built interface and abstract class

--- Week1/HelloAbstract/src/com/revature/models/Pokemon.java
+++ Week1/HelloAbstract/src/com/revature/models/Pokemon.java
@@ -7,7 +7,7 @@ public abstract class Pokemon implements Creature {
 
 	//abstract classes can have variables
 	boolean fainted = false; //we'll give this a default value
-	public String type; //this value will be null by default and filled in each unique pokemon object.
+	public int level = 5; //this value will be 5 by default
 	
 	//concrete method - has an implementation (aka method body)
 	public void flee() {
@@ -17,8 +17,12 @@ public abstract class Pokemon implements Creature {
 	//abstract method - is unimplemented
 	public abstract void fight(); //every unique Class that extends the Pokemon Abstract Class will need to implement this method.
 
-	//should I maybe add a constructor? the default no args constructor is here for now
+	//another abstract method
+	public abstract void gainLevel();
 	
+	
+	//should I maybe add a constructor? the default no args constructor is here for now
+	//and that should be sufficient because the variables here already have default values.
 	
 	//abstract classes can't be instantiated but they can have constructors? 
 	//Yup, any subclasses will have access to them so they can still be used.
