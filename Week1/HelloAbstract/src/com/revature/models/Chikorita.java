6e486cfe243cdfc6c1940e3f7e037b82fa71a479
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-01 10:54:52 -0400

    Class done in HelloAbstract

--- Week1/HelloAbstract/src/com/revature/models/Chikorita.java
+++ Week1/HelloAbstract/src/com/revature/models/Chikorita.java
@@ -5,20 +5,41 @@ package com.revature.models;
 //Pokemon also implements Creature... so Chikorita inherits from both Pokemon AND creature. so it has access to all fields/methods
 public class Chikorita extends Pokemon{
 
-	//ben will add variables and some constructors************************************************88dfjkdsgajvdshdskjhdsjh
+	//some variables unique to Chikoritas
+	public String name = "Chikorita";
+	public String type = "Grass";
 	
 	//This is the eat method from the CREATURE INTERFACE
 	@Override
 	public void eat(String food) {
-		// TODO Auto-generated method stub
-		
+		System.out.println(this.name + " eats a " + food);
 	}
 
 	//This is the fight method from the POKEMON ABSTRACT CLASS
 	@Override
 	public void fight() {
-		// TODO Auto-generated method stub
-		
+		System.out.println(this.name + " used vine whip!!");
+	}
+
+	//This is the gainLevel method from the POKEMON ABSTRACT CLASS
+	@Override
+	public void gainLevel() {
+		this.level++; //increment the level by 1
+		System.out.println(this.name + " has grown to level: " + this.level);
+	}
+
+	//no-args constructor, thanks to super(); we call the default no-args constructor of Pokemon... Which calls the one from Object
+	public Chikorita() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	//all-args constructor... we probably won't need this since every variable is initialized already
+	//but all-args are always useful for user flexibility... Maybe they want to give a nickname to their Pokemon.
+	public Chikorita(String name, String type) {
+		super();
+		this.name = name;
+		this.type = type;
 	}
 
 	
