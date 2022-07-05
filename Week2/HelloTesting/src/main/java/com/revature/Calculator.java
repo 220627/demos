6bff8a3ca6b75f42f6fa2f92f6e0816d90ed8118
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-05 16:59:52 -0400

    used JUnit in HelloTesting after much struggle

---
+++ Week2/HelloTesting/src/main/java/com/revature/Calculator.java
@@ -0,0 +1,17 @@
+package com.revature;
+
+public class Calculator {
+
+	public int add(int i, int j) {
+		return i + j;
+	}
+
+	public int subtract(int i, int j) {
+		return i - j;
+	}
+
+	public int divide(int i, int j) {
+		return i / j;
+	}
+	
+}
