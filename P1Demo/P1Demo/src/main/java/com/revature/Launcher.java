c6ffb75e5a04ee8f95774a823af4e6d659b53295
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-07 15:12:18 -0400

    ConnectionUtil in P1Demo

---
+++ P1Demo/P1Demo/src/main/java/com/revature/Launcher.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("========Welcome to the Krusty Krab Employee Management System========");
+		
+	}
+	
+}
