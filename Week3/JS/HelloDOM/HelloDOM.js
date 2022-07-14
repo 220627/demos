60d9090246f7fdd80ac4a79b0ac1d7d014eb94be
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-14 16:15:40 -0400

    events in HelloDom

--- Week3/JS/HelloDOM/HelloDOM.js
+++ Week3/JS/HelloDOM/HelloDOM.js
@@ -16,3 +16,37 @@ firstDiv.setAttribute("style", "background-color:pink; font-family:calibri")
 
 console.log("====================================(Events)")
 
+//Assigning a variable that applies to our button
+let myButton = document.getElementById("b1")
+
+//event listeners take in the event we're listening for, and the function to execute.
+myButton.addEventListener("click", domFunc)
+
+//declare the domFunc
+function domFunc() {
+    //I want to change some of the content we see on our webpage
+    myButton.textContent = "Thanks for the click!" //textContent accesses the element's text
+
+    //we can add an eventListener after DOM selection
+    document.getElementById("p3").textContent = "NOW I HAVE CONTENT >:)"
+}
+
+//there a shorter to add eventListeners
+
+//get the h3 using DOM selection
+let header = document.getElementById("header")
+
+//every time we mouse over our header the hover function will execute
+header.onmouseover = hoverFunc
+
+function hoverFunc() {
+    header.textContent = "don't touch me!!"
+}
+
+//and then, every time our mouse leaves the header, the release function will execute
+
+header.onmouseleave = releaseFunc
+
+function releaseFunc(){
+    header.textContent = "why did you leave me :("
+}
