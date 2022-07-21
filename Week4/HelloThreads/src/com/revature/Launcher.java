ba15aba05a133778ee065a2a915b75ab1a16bb74
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-21 16:11:57 -0400

    HelloThreads

--- Week4/HelloThreads/src/com/revature/Launcher.java
+++ Week4/HelloThreads/src/com/revature/Launcher.java
@@ -2,4 +2,30 @@ package com.revature;
 
 public class Launcher {
 
+	public static void main(String[] args) {
+		
+		//Instantiate two new Threads so we can see multi-threading at work
+		MyThread t1 = new MyThread();
+		MyThread t2 = new MyThread();
+		
+		
+		//the start() method actually runs our Threads
+		t1.start();
+		t2.start();
+		
+		//what if we wanted each Thread to sleep for longer?
+		//Best solution is probably make a variable in the Thread class, which you can set per object
+		//or we could overload the run() method to take in an int for the sleep variable
+		
+		//make a couple new threads to see the order they run in
+		MyThread t3 = new MyThread();
+		MyThread t4 = new MyThread();
+		
+		t3.start();
+		t4.start();
+		
+		//The order changes as they go! These threads are running concurrently, independent of each other
+		
+	}
+	
 }
--- Week4/HelloThreads/src/com/revature/MyThread.java
+++ Week4/HelloThreads/src/com/revature/MyThread.java
@@ -1,5 +1,36 @@
 package com.revature;
 
-public class MyThread {
+//by extending the Thread class, this class is now a Thread and we can override the run() method
+public class MyThread extends Thread {
 
+	//we can now define whatever process we want this Thread Class to have
+	@Override
+	public void run() {
+		
+		for(int i = 0; i < 10; i++) {
+			
+			//This String will be populated with the name of the current Thread we're in
+			StringBuilder name = new StringBuilder(Thread.currentThread().getName());
+			
+			//append a working message to the end of our StringBuffer	
+			name.append(" is working...");
+			
+			//print out the StringBuffer so that we can see the Threads working
+			System.out.println(name);
+			
+			//Thread.sleep() forces a thread to wait x amount of milliseconds
+			//this method potentially throws an exception, so we need this try/catch
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		System.out.println(Thread.currentThread().getName() + " finished!");
+		
+	} //end of run() method
+	
 }
