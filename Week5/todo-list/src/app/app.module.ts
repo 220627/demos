2d5690fde4330589bdbb8981db198e6caa7aac90
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-26 17:02:49 -0400

    todos banner in progress

--- Week5/todo-list/src/app/app.module.ts
+++ Week5/todo-list/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosBannerComponent } from './components/todos-banner/todos-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosBannerComponent
   ],
   imports: [
     BrowserModule,
