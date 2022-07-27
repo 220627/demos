6be5cbded99c58a17ea972e0035aabdb286c3fb5
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-27 12:26:54 -0400

    todos home component in progress

--- Week5/todo-list/src/app/components/todos-home/todos-home.component.ts
+++ Week5/todo-list/src/app/components/todos-home/todos-home.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosHomeComponent implements OnInit {
 
+  //4 todo objects that we will render onto our webpage
+  todo1 = {
+    id: 1,
+    task: "wake up",
+    status: "Incomplete"
+  }
+
+  todo2 = {
+    id: 2,
+    task: "grab a brush",
+    status: "Incomplete"
+  }
+
+  //An Array filled with the above todo objects
+  //we're going to call from this in our HTML to render our todo list
+  todosArray = [this.todo1, this.todo2]
+
+
+
   constructor() { }
 
   ngOnInit(): void {
