9dc93089ec8ff8ef0adc5c1278d9f57fee9fce69
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-27 11:37:54 -0400

    todos home component in progress

--- Week5/todo-list/src/app/app-routing.module.ts
+++ Week5/todo-list/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosHomeComponent } from './components/todos-home/todos-home.component';
 
-const routes: Routes = [];
+//remember this routes array is what lets us control what component is rendered and when
+//based on the URL we're on, certain components will render
+const routes: Routes = [
+
+  {
+    path: "todos", //path is the URL to see the given component
+    component: TodosHomeComponent //component is the component that gets rendered given a certain URL
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Week5/todo-list/src/app/app.module.ts
+++ Week5/todo-list/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosBannerComponent } from './components/todos-banner/todos-banner.component';
+import { TodosHomeComponent } from './components/todos-home/todos-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosBannerComponent
+    TodosBannerComponent,
+    TodosHomeComponent
   ],
   imports: [
     BrowserModule,
