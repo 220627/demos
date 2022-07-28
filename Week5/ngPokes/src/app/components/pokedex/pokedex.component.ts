999d1ef2882013b9041c1fd1d4416b8c14f9ebab
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-28 16:39:30 -0400

    ngPokes setup for HTTP calls with Observables

--- Week5/ngPokes/src/app/components/pokedex/pokedex.component.ts
+++ Week5/ngPokes/src/app/components/pokedex/pokedex.component.ts
@@ -10,9 +10,13 @@ export class PokedexComponent implements OnInit {
   //this variable gets filled by the user input in the pokedex html
   public input:number = 0;
 
-
-
   constructor() { }
+
+  //This is the function that gets our Pokemon object using a functino from the Pokemon Service
+  //In order to get data from the Observable returned by the Service, we need to SUBSCRIBE
+  getPoke(){
+    
+  }
 
   ngOnInit(): void {
   }
