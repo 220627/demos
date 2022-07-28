6b89549ac1ef2aad28863cc6c8076591af011b94
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-28 16:25:55 -0400

    ngPokes pokemon class

---
+++ Week5/ngPokes/src/app/models/pokemon.ts
@@ -0,0 +1,12 @@
+//You can think of the models folder in Angular like the models package Java
+//It holds Classes that we intend to make object of
+export class Pokemon {
+
+    //this constructor will initialize the Pokemon's variables when we get it from the API
+    constructor(
+        public name: String,
+        public types: Object,
+        public sprite: Object
+    ){}
+
+}
