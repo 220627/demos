ae09a5c32412af64fcd1efa4571efa89082af9d4
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-28 15:34:54 -0400

    ngPokes pokedex skeleton

--- Week5/ngPokes/src/app/app.module.ts
+++ Week5/ngPokes/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { PokedexComponent } from './components/pokedex/pokedex.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Week5/ngPokes/src/app/components/pokedex/pokedex.component.ts
+++ Week5/ngPokes/src/app/components/pokedex/pokedex.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokedexComponent implements OnInit {
 
+  //this variable gets filled by the user input in the pokedex html
+  public input:number = 0;
+
+
+
   constructor() { }
 
   ngOnInit(): void {
