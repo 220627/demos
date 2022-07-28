4195ad8fe41a17b2e4e6ddb4462a10af2369a812
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-07-28 14:46:20 -0400

    ngPokes routes done

--- Week5/ngPokes/src/app/app-routing.module.ts
+++ Week5/ngPokes/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokedexComponent } from './components/pokedex/pokedex.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-const routes: Routes = [];
+//Remember, the Routes Array is how we set up routing for components
+//When we go to a certain URL path, this Array will be consulted to determine what component is rendered
+const routes: Routes = [
+
+  {
+    path: "",
+    component: WelcomeComponent
+  },
+
+  {
+    path: "pokedex",
+    component: PokedexComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
