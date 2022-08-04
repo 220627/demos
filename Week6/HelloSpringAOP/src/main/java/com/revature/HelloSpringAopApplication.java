c152a5384badf92eb14c4a830bc4c008553ad794
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-08-04 17:00:14 -0400

    SpringAOP lol

--- Week6/HelloSpringAOP/src/main/java/com/revature/HelloSpringAopApplication.java
+++ Week6/HelloSpringAOP/src/main/java/com/revature/HelloSpringAopApplication.java
@@ -3,11 +3,19 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.revature.models.Avenger;
+
 @SpringBootApplication
 public class HelloSpringAopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringAopApplication.class, args);
+		
+		//Make an Avenger object (we can see our @Before advices run here)
+		Avenger a = new Avenger(10, "Frozone", "Freeze", "Lucious", "Best", 6000);
+		
+		
+		
 	}
 
 }
--- Week6/HelloSpringAOP/src/main/java/com/revature/aspects/LoggingAspect.java
+++ Week6/HelloSpringAOP/src/main/java/com/revature/aspects/LoggingAspect.java
@@ -1,6 +1,10 @@
 package com.revature.aspects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component //once again making a class a Spring Bean
@@ -8,6 +12,23 @@ import org.springframework.stereotype.Component;
 //It's so much cleaner in the long run to put all of our log stuff in one place, which is the point of SpringAOP.
 public class LoggingAspect {
 
+	//A Logger object... so we can do Logging.
+	private static Logger log = LogManager.getLogger();
+	
+	//in an Aspect, we have methods called ADVICES. Each "advice" will be responsible for logging certain things.
+	
+	//This advice will run some logs BEFORE any method in our models package runs.
+	//this happens thanks to the within() and the name of the package you want to scan
+	//"Before any method in the models package runs, execute this code"
+	@Before("within(com.revature.models.*)")
+	public void logModelMethods(JoinPoint jp) {//A JoinPoint is where an advice COULD be injected. In this case, models
+		
+		//getTarget() returns the object getting called. 
+		//getSignature() returns the method signature getting called.
+		log.info(jp.getTarget() + " invoked " + jp.getSignature());
+		
+	}
+	
 	
 	
 }
