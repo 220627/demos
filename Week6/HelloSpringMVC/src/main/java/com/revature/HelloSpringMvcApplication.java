30a0a26168219ae25e276f2a7b98470fd2491c13
benp3837 <petruzziellbj@vcu.edu>
2023-01-09 17:27:30 -0500

    Update HelloSpringMvcApplication.java

--- Week6/HelloSpringMVC/src/main/java/com/revature/HelloSpringMvcApplication.java
+++ Week6/HelloSpringMVC/src/main/java/com/revature/HelloSpringMvcApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.revature")
 public class HelloSpringMvcApplication {
 
 	public static void main(String[] args) {
